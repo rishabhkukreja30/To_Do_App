f9c1f94b8077a34591e962ba04ed61b307608eb5
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2023-12-08 17:51:20 +0530

    added a simple hello world controller

--- todoapp/src/main/java/com/example/firstwebapp/todoapp/hello/SayHelloController.java
+++ todoapp/src/main/java/com/example/firstwebapp/todoapp/hello/SayHelloController.java
@@ -1,2 +1,15 @@
-package com.example.firstwebapp.todoapp.hello;public class SayHelloController {
+package com.example.firstwebapp.todoapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! How are you?";
+    }
 }
